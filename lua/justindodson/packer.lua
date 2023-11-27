382b4b8026480478619a31d9c2d30f292e649b8c
Justin Dodson <justin.dodson@agoradata.com>
2023-11-27 15:36:36 -0600

    add statusline

--- lua/justindodson/packer.lua
+++ lua/justindodson/packer.lua
@@ -8,7 +8,7 @@ return require('packer').startup(function(use)
 	use 'wbthomason/packer.nvim'
 
 	use {
-		'nvim-telescope/telescope.nvim', tag = '0.1.1',
+		'nvim-telescope/telescope.nvim', tag = '0.1.4',
 		-- or                            , branch = '0.1.x',
 		requires = { {'nvim-lua/plenary.nvim'} }
 	}
@@ -122,5 +122,7 @@ return require('packer').startup(function(use)
         { 'rmagatti/goto-preview', config = function() require('goto-preview').setup {} end } --optional
     }
 }
+    use 'freddiehaddad/feline.nvim'
+    use "lewis6991/gitsigns.nvim"
 
 end)
